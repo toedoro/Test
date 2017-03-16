ed9d450a013f71a2a833892f1985e67b232b6030
tdelacerna <tdelacerna@ms3-inc.com>
2017-03-16 22:47:56 +0800

    init: gateway

---
+++ bisaya/bisaya-gateway/src/main/java/ph/com/montrichard/bisaya/gateway/BisayaGatewayApplication.java
@@ -0,0 +1,12 @@
+package ph.com.montrichard.bisaya.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisayaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisayaGatewayApplication.class, args);
+	}
+}
---
+++ bisaya/bisaya-gateway/src/test/java/ph/com/montrichard/bisaya/gateway/BisayaGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package ph.com.montrichard.bisaya.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BisayaGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
