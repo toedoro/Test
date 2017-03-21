497bf2618994db02427ea2fac457262fb482aace
teodoro.delacerna (FPHI.DMO) <teodoro.delacerna@fsoft.com.vn>
2017-03-21 13:57:17 +0800

    add batch file for running spring-boot and config

---
+++ bisaya/bisaya-registry/spring-boot.bat
@@ -0,0 +1,3 @@
+:: mvn clean install spring-boot:run -Dmaven.test.skip=true -Drun.jvmArguments="-Xmx64M"
+call mvn clean install -Dmaven.test.skip=true -Drun.jvmArguments="-Xmx64M" 
+java -jar target\bisaya-registry-1.0.jar
